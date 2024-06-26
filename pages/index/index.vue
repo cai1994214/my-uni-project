8de0186e5445d15be6ac8da30978ebb608083503
renjie.cai <renjie.cai@wzip.com>
2024-06-26 21:16:33 +0800

    fix: 首页轮播

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,41 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+	<swiper class="banner" indicator-dots circular :autoplay="true">
+		<swiper-item v-for="{url, id} in pictures" :key="id">
+			<image @tap="onPreviewIamge(url)" :src="url"/>
+		</swiper-item>
+	</swiper>
 </template>
 
 <script>
 	export default {
 		data() {
+			const path = 'https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/'
 			return {
-				title: 'Hello vue3 123 '
+				pictures: [
+					{id: 1, url: path+`goods_preview_1.jpg`},
+					{id: 2, url: path+`goods_preview_2.jpg`},
+					{id: 3, url: path+`goods_preview_3.jpg`},
+					{id: 4, url: path+`goods_preview_4.jpg`},
+				]
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			onPreviewIamge(url) {
+				uni.previewImage({
+					urls: this.pictures.map(e => e.url),
+					current: url
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.banner, .banner image {
+		width: 750rpx;
+		height: 750rpx;
 	}
 </style>
